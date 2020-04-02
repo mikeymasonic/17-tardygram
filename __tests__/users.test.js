e0ceea0f008912e5dc4097c409448e4515ef7b7c
Michael Romay <amerror@gmail.com>
2020-04-02 09:52:50 -0700

    user tests and more aggregates

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 
 describe('users routes', () => {
 
-  it('gets the 10 users with the most total comments', async() => {
+  it('gets the 10 users with the most total comments on their posts', async() => {
     return request(app)
       .get('/api/v1/users/popular')
       .then(res => {
@@ -14,6 +14,32 @@ describe('users routes', () => {
           _id: expect.any(String),
           username: expect.any(String),
           totalComments: expect.any(Number)
+        });
+      });
+  });
+
+  it('gets the 10 users with the most grams', async() => {
+    return request(app)
+      .get('/api/v1/users/prolific')
+      .then(res => {
+        expect(res.body).toHaveLength(10);
+        expect(res.body).toContainEqual({
+          _id: expect.any(String),
+          username: expect.any(String),
+          totalGrams: expect.any(Number)
+        });
+      });
+  });
+
+  it('gets the 10 users with the most total comments', async() => {
+    return request(app)
+      .get('/api/v1/users/leader')
+      .then(res => {
+        expect(res.body).toHaveLength(10);
+        expect(res.body).toContainEqual({
+          _id: expect.any(String),
+          username: expect.any(String),
+          totalCommentsWritten: expect.any(Number)
         });
       });
   });
